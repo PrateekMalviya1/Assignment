3d08e3b8ea52f8cdaa8354eb3612b34794c842de
PrateekMalviya1 <malviyaprateek93@gmail.com>
2025-08-31 23:03:40 +0530

    add Port Number in Connection commit

--- src/DatabaseConnection.js
+++ src/DatabaseConnection.js
@@ -7,7 +7,8 @@ export const connectDb = async () => {
             host: process.env.MYSQLHOST,
             user: process.env.MYSQLUSER,
             password: process.env.MYSQLPASSWORD,
-            database: process.env.MYSQLDATABASE
+            database: process.env.MYSQLDATABASE,
+            port: '3306'
         })
     }
     return connection;
