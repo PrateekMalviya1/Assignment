0e595ce73a0ff487f5d5d344f67a99fcae9ddd47
PrateekMalviya1 <malviyaprateek93@gmail.com>
2025-09-01 13:19:26 +0530

    Fixed Header Scroll

--- src/app/Pages/ShowSchool/page.jsx
+++ src/app/Pages/ShowSchool/page.jsx
@@ -17,25 +17,28 @@ export default function page() {
     getData()
   }, [])
   return (
-    <div className='dark:bg-neutral-800 dark:text-neutral-100 bg-neutral-100 text-neutral-800 min-h-[100vh]'>
+    <div>
       <header className='shadow shadow-neutral-300 dark:shadow-neutral-700 drop-shadow-xl mb-2'>
         <Header />
       </header>
+      <div className='dark:bg-neutral-800 dark:text-neutral-100 bg-neutral-100 text-neutral-800 min-h-[100vh]'>
 
-      <div className='max-w-[1320px] mx-auto px-[20px]'>
-        <h2 className='text-center text-[30px] my-[20px] font-medium'> School Records </h2>
-        
-        <div className='grid gap-3 py-1 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-          {
-            (data.length > 0) ?
-              data.map((value, index) => {
-                return (
-                  <Card key={index} name={value.name} address={value.address} city={value.city} state={value.state} contact={value.contact} imageName={value.image} email={value.email_id} />
 
-                )
-              })
-              : <div className='m-2 w-[90%] border shadow-2xl h-[200px] rounded-2xl text-center'> ... Plese Wait</div>
-          }
+        <div className='max-w-[1320px] mx-auto px-[20px]'>
+          <h2 className='text-center text-[30px] my-[20px] font-medium'> School Records </h2>
+
+          <div className='grid gap-3 py-1 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {
+              (data.length > 0) ?
+                data.map((value, index) => {
+                  return (
+                    <Card key={index} name={value.name} address={value.address} city={value.city} state={value.state} contact={value.contact} imageName={value.image} email={value.email_id} />
+
+                  )
+                })
+                : <div className='m-2 w-[90%] border shadow-2xl h-[200px] rounded-2xl text-center'> ... Plese Wait</div>
+            }
+          </div>
         </div>
       </div>
     </div>
